5d152caef2683a042877ffab2592134fb672ddc0
HuangDi73 <dahaka0@mail.ru>
2025-03-18 14:45:28 +0300

    Добавлен кастомный роутер и простая реализация сервера.

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	router := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
+		Handler: router,
+	}
+
+	log.Printf("Server is running in http://localhost%s", server.Addr)
+	server.ListenAndServe()
 }
