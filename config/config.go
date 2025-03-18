3b925460b4cd7417644763870e068353d1a74c96
HuangDi73 <dahaka0@mail.ru>
2025-03-18 14:42:35 +0300

    Добавлен файл конфигурации с возможностью чтения переменных окржения.

--- config/config.go
+++ config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Db   DbConfig
+	Auth AuthConfig
+}
+
+type DbConfig struct {
+	Dsn string
+}
+
+type AuthConfig struct {
+	Secret string
+}
+
+func Load() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading env file, using the default one")
+	}
+	return Config{
+		Db: DbConfig{
+			Dsn: os.Getenv("DSN"),
+		},
+		Auth: AuthConfig{
+			Secret: os.Getenv("SECRET"),
+		},
+	}
+}
