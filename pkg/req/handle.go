4ef3143d2219b66436cb73d823d7c277b9d8867f
HuangDi73 <dahaka0@mail.ru>
2025-10-14 11:30:07 +0300

    Реализована логика обработки тела запроса

--- pkg/req/handle.go
+++ pkg/req/handle.go
@@ -1,1 +1,20 @@
 package req
+
+import (
+	"net/http"
+	"url-shortener/pkg/res"
+)
+
+func HandleBody[T any](w http.ResponseWriter, r *http.Request) (*T, error) {
+	body, err := Decode[T](r.Body)
+	if err != nil {
+		res.Json(w, err.Error(), http.StatusBadRequest)
+		return nil, err
+	}
+	err = Validate(body)
+	if err != nil {
+		res.Json(w, err.Error(), http.StatusBadRequest)
+		return nil, err
+	}
+	return body, nil
+}
