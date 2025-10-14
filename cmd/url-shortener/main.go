af1c51ce7dfabf8f655c21aac4cd2775e20cc058
HuangDi73 <dahaka0@mail.ru>
2025-10-14 10:55:16 +0300

    Создан базовый сервер с кастомным роутером

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: mux,
+	}
+
+	log.Printf("Server is running on http://localhost%s", server.Addr)
+	server.ListenAndServe()
 }
